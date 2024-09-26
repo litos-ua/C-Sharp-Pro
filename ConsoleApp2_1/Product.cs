82f73b75d05427a0730a05f40c0c7410447a7855
litos-ua <andrzejlitkiewicz@gmail.com>
2024-09-26 08:19:20 +0200

    Add 2.1_1 and README

---
+++ ConsoleApp2_1/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp2_1
+{
+    class Product
+    {
+        public string ProductName { get; set; }
+        public string ProductCategory { get; set; }
+        public string ProductDescription { get; set; }
+        public int ProductPrice { get; set; }
+        public int ProductQuantity { get; set; }
+        public int ProductDiscount { get; set; }
+
+        public Product(string productName, int productPrice, int productQuantity, int productDiscount = 0, string productCategory = "", string productDescription = "")
+        {
+            ProductName = productName;
+            ProductPrice = productPrice;
+            ProductQuantity = productQuantity;
+            ProductDiscount = productDiscount;
+            ProductCategory = productCategory;
+            ProductDescription = productDescription;
+        }
+
+        public int QuantityDiff(int quantity)
+        {
+            ProductQuantity -= quantity;
+            return ProductQuantity;
+        }
+    }
+
+}
---
+++ ConsoleApp2_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp2_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Money account = new Money();
+            Product apple = new Product("Apple", 2, 50, 0); // Цена 2 евро за штуку, в наличие 50 шт., без скидки
+            Product mango = new Product("Mango", 5, 7, 1); // Цена 5 евро за штуку, в наличие 7 шт., 1 евро скидки
+
+
+            account.AddMoney(20, 50); // Добавляем на счет 20 евро 50 центов
+            account.BuyProduct(apple, 5); // Покупаем 5 яблок
+            account.BuyProduct(mango, 5); // Покупаем 5 манго - недостаточно средств
+            account.HowMuchMoney(); //Проверяем счет
+            account.AddMoney(15, 20); // Добавляем на счет 15 евро 20 центов
+            account.BuyProduct(mango, 5); //Повторяем покупку манго - удачно
+        }
+    }
+}
+
+
