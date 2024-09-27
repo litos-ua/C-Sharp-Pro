a64eeea4ccd1b0d4856f830458f51d292870b21b
litos-ua <andrzejlitkiewicz@gmail.com>
2024-09-27 12:52:00 +0200

    Interfases. Part_2. Create interface IMath.

---
+++ ConsoleApp3_2/Mylist.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleApp3_2;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp3_2
+{
+    internal class MyList : IOutput
+    {
+        private List<int> myList;
+
+        public MyList(List<int> list)
+        {
+            myList = list;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Elements of the list:");
+            foreach (var element in myList)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public void Show(string info)
+        {
+            Console.WriteLine(info);
+            Show();
+        }
+    }
+}
---
+++ ConsoleApp3_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApp3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intNumbers = new List<int> { 197, 12, -3, 451, 5, 77, -89, 0, 11, 122, 9789, 0, 7, 123, 255 };
+            MyList myList = new MyList(intNumbers);
+
+            myList.Show();
+            myList.Show($"The list of integers ({intNumbers.Count}):");
+        }
+    }
+}
