bdcc60883bb62c864df54a6f1afc3c77650be99a
litos-ua <andrzejlitkiewicz@gmail.com>
2024-09-28 14:37:06 +0200

    Interfases. Some changes i comments once more.

--- ConsoleApp3_3/Program.cs
+++ ConsoleApp3_3/Program.cs
@@ -1,77 +1,8 @@
-﻿//using System;
-//using System.Collections;
-//using System.Collections.Generic;
-
-//namespace ConsoleApp3_3
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            List<int> intNumbers = new List<int> { 197, 12, -3, 451, 5, 77, -89, 0, 11, 122, 9789, 0, 7, 123, 255 };
-//            MyList myList = new MyList(intNumbers);
-
-
-
-//            // Перевірка блокировки створення пустого листа
-//            try
-//            {
-//                List<int> intNumbersEmpty = new List<int> { };
-//                MyList myListEmpty = new MyList(intNumbersEmpty);
-//            }
-//            catch (ArgumentException ex)
-//            {
-//                Console.WriteLine(ex.Message);
-//            }
-
-
-//            /* З А В Д А Н Н Я 1 */
-//            Console.WriteLine("Task 3.1");
-//            myList.Show();
-//            myList.Show($"The list of integers ({intNumbers.Count}):");
-//            /*--------------------------------------------------------------*/
-
-
-
-//            /* З А В Д А Н Н Я 2 */
-//            Console.WriteLine("\n Task 3.2");
-//            Console.WriteLine($"Maximum value of the list: {myList.Max()}");
-//            Console.WriteLine($"Minimum value of the list: {myList.Min()}");
-//            Console.WriteLine($"Average value of the list: {myList.Avg()}");
-
-//            Console.WriteLine("Enter the number.");
-//            string searchParamStr = Console.ReadLine();
-
-//            // Перевірка чи належить введене число до списку.
-//            if (Int32.TryParse(searchParamStr, out int searchParam))
-//            {
-//                Console.WriteLine(myList.Search(searchParam)
-//                     ? $"The element '{searchParam}' has been found." :
-//                       $"The element '{searchParam}' hasn't been found.");
-//            }
-//            else
-//            {
-//                Console.WriteLine("Invalid number.");
-//            }
-//            /*-------------------------------------------------------------*/
-
-//            /* З А В Д А Н Н Я 3 */
-
-//            myList.SortAsc();
-//            myList.SortDesc();
-//            Console.WriteLine("\nParameter is true");
-//            myList.SortByParam(true);
-//            Console.WriteLine("\nParameter is false");
-//            myList.SortByParam(false);
-
-//        }
-//    }
-//}
+﻿
 
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Security.Cryptography;
 
 namespace ConsoleApp3_3
 {
