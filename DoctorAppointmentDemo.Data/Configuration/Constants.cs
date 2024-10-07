cf4700188a49334c8708420aa4231ee150945ba4
litos-ua <andrzejlitkiewicz@gmail.com>
2024-10-07 09:55:38 +0200

    Overloading. Delete some files.

--- DoctorAppointmentDemo.Data/Configuration/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MyDoctorAppointment.Data.Configuration
-{
-    public static class Constants
-    {
-        // заменить на путь валидный для вашей директории на пк (в будущем будем использовать относительный путь)
-        public const string AppSettingsPath = "C:\\Training\\Home work\\C#\\DoctorAppointmentDemo\\DoctorAppointmentDemo.Data\\Configuration\\appsettings.json";
-    }
-}
