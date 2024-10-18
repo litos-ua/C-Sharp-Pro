f6c38c3e4f5a029ab4da5dcbc10b3376afd4b9be
litos-ua <andrzejlitkiewicz@gmail.com>
2024-10-18 18:35:21 +0300

    DoctorAppointment Ver_3.1 Change some path.

--- DoctorAppointmentDemo.Data/Configuration/Constants.cs
+++ DoctorAppointmentDemo.Data/Configuration/Constants.cs
@@ -1,8 +1,20 @@
 ﻿namespace MyDoctorAppointment.Data.Configuration
 {
+    //public static class Constants
+    //{
+    //    // заменить на путь валидный для вашей директории на пк (в будущем будем использовать относительный путь)
+    //    public const string AppSettingsPath = "C:\\Training\\Home work\\C#\\DoctorAppointmentDemo\\DoctorAppointmentDemo.Data\\Configuration\\appsettings.json";
+    //}
+
     public static class Constants
     {
-        // заменить на путь валидный для вашей директории на пк (в будущем будем использовать относительный путь)
-        public const string AppSettingsPath = "C:\\Training\\Home work\\C#\\DoctorAppointmentDemo\\DoctorAppointmentDemo.Data\\Configuration\\appsettings.json";
+        public static string AppSettingsPath
+        {
+            get
+            {
+                return PathHelper.GetDatabaseFilePath("DoctorAppointmentDemo.Data/Configuration/appsettings.json");
+            }
+        }
     }
 }
+
